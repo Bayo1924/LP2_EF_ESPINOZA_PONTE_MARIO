b13d450a555a59e201bd22f0d76807f09ba026cc
Mario Espinoza <maed180103@gmail.com>
2024-07-04 07:41:00 -0500

    login hecho

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemporalApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemporalApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
